ca262c2c866f817814689c3083a3443529cb7f76
Manthan Patel <0xmanthanpatel@gmail.com>
2025-03-29 02:08:46 +0530

    chore: update environment variable files for backend and initialize frontend sample.env

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,7 +4,6 @@ dotenv.config();
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
-export const NODE_ENV = process.env.NODE_ENV;
 
 export const SMTP_HOST = process.env.SMTP_HOST;
 export const SMTP_PORT = process.env.SMTP_PORT;
